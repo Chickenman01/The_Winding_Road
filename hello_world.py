7ca4381d09d04cb5de8c8c3baf55a4d00a05af66
Rob <chicken_man_rob@hotmail.com>
2019-08-27 21:42:48 -0400

    more stuff

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
-print ("Hello World")
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?')     # Ask for user age
+myAge = input()
+print(' You will be ' + str(int(myAge) + 1) + ' in a year.')

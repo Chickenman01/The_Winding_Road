56d71a401df231c8681ff0fa297cec60b5cb1470
Martin <Robert.Martin@umms.umm.edu>
2019-12-30 16:39:47 -0500

    Added Class.py

--- player_classes.py
+++ player_classes.py
@@ -1,1 +1,15 @@
 # this is where player classes will go along with their pertinent stats
+
+Priest = '' # Contains main stats for Priest class
+
+Mage = '' # Contains main stats for Mage class
+
+Warrior = '' # Contains main stats for Warrior class
+
+Thief = '' # Contains main stats for Thief class
+
+# Basic stats should be:
+# Health or HP *Max health is 3*
+# Attack or AP *Max attack is 3*
+# Chance or CH *This is a percentage from 0% to 100%*
+# Energy or EN *This is both the measure for mana and energy*
